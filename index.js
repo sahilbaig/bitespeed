3f281d261c2981241f164235fd45faa4326ada0b
Sahil Baig <sahilbaig.me@gmail.com>
2024-05-23 22:48:34 +0530

    sample input and otuput checked : Works

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const PORT=8000
 
 const app = express()
 
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get("/" , (req, res)=>{
-    res.send("Intial setup", 200)
+    console.log(req.body)
+    //req format = {email?: sting , phoneNumber?: number }
+
+    // Sample Output
+    const mockdata={
+		"contact":{
+			"primaryContatctId": 1,
+			"emails": ["sa","sb"], // first element being email of primary contact 
+			"phoneNumbers": [1,2,3], // first element being phoneNumber of primary contact
+			"secondaryContactIds": [2,3] // Array of all Contact IDs that are "secondary" to the primary contact
+		}
+	}
+  
+    res.json(mockdata);
 
 })
 
